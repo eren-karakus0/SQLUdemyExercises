d2910fc4458b71f4931193447604041a0498e2e8
Muhammet Eren Karakuş <erenkar950@gmail.com>
2024-03-24 17:46:37 +0300

    SQL Udemy Exercises
    SQL Udemy Exercises

---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+--Create Table TBLDERSLER
+--(
+--DERSID tinyint,
+--DERSAD varchar(20)
+--)
+
+--Alter Table TBLDERSLER
+--Drop Column KONTENJAN
+
+Select * From Tablo_Ogrenciler Where OGRAD = 'Ali'
